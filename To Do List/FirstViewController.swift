7c2b86c1b7dd3182896a3382b3e00568358d137e
Phil Araujo <araujophil@gmail.com>
2016-07-23 17:03:34 -0400

    allow delete functionality while maintaining permanent storage

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -57,6 +57,25 @@ class FirstViewController: UIViewController, UITableViewDelegate {
         
     }
     
+    // this will allow the user the ability to delete an item by swiping once they are no longer in need of it
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        // this if statement will check and will be called if it detects that the user has swiped on an item to delete it
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            
+            // this .removeAtIndex snippet will detect what item at what the row user has swiped and will delete it
+            toDo.removeAtIndex(indexPath.row)
+            
+            // this will again update the permanent storage. this time it will have deleted the selected item that the user chose to delete
+            NSUserDefaults.standardUserDefaults().setObject(toDo, forKey: "toDo")
+            
+            // and this will also again update the list in real time
+            toDoTable.reloadData()
+            
+        }
+    }
+    
+    
     // this snippet will update the app list in real time as opposed to having the list updated everytime the user closes and reopens the app. therefore the user can switch back and forth from view controllers with updated lists
     override func viewDidAppear(animated: Bool) {
         
@@ -65,6 +84,21 @@ class FirstViewController: UIViewController, UITableViewDelegate {
         
     }
 
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
